a66c2b885558168ab760b69fb642c7ee2225bd5a
qasim <bqasim381@gmail.com>
2018-05-12 17:57:18 -0400

    main screen layout

--- InventorySystem/src/application/Main.java
+++ InventorySystem/src/application/Main.java
@@ -1,46 +1,25 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
 public class Main extends Application {
-    public static void main(String[] args) {
-        launch(args);
+  
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        primaryStage.setTitle("Hello World");
+//        primaryStage.show();
+        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.show();
     }
     
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Main Screen");
-        Button add = new Button();
-        add.setText("Add");
-        
-        Button modify = new Button();
-        modify.setText("Modify");
-        
-        Button delete = new Button();
-        delete.setText("Delete");
-        
-        Button search = new Button();
-        search.setText("Search");
-//        btn.setOnAction(new EventHandler<ActionEvent>() {
-// 
-//            @Override
-//            public void handle(ActionEvent event) {
-//                System.out.println("Hello World!");
-//            }
-//        });
-        add.setOnAction(event ->
-        	System.out.println("Hellow World!")
-        		);
-        
-        StackPane root = new StackPane();
-        root.getChildren().addAll(add, modify, delete, search);
-        primaryStage.setScene(new Scene(root, 1000, 750));
-        primaryStage.show();
+    public static void main(String[] args) {
+        launch(args);
     }
 }
